d275e4afe86f7c0e2ff727a3c2212dda741d720e
quochungvu <vuquochung84@gmail.com>
2025-02-24 10:47:51 +0700

    update

--- mylibrary/src/main/java/com/iccom/demolib/Helper.java
+++ mylibrary/src/main/java/com/iccom/demolib/Helper.java
@@ -2,8 +2,12 @@ package com.iccom.demolib;
 
 import android.util.Log;
 
+import com.iccom.demolib.ex.Person;
+
 public class Helper {
     public static void printLog(String tag, String msg){
-        Log.d(tag,msg);
+        Person person = new Person();
+        person.setFullName("abc");
+        Log.d(tag,person.getFullName());
     }
 }
---
+++ mylibrary/src/main/java/com/iccom/demolib/ex/Person.java
@@ -0,0 +1,26 @@
+package com.iccom.demolib.ex;
+
+public class Person {
+    private String fullName;
+    private int age;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullname(){
+        return fullName;
+    }
+}
